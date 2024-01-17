2e2ccc9bb9ae631e7d65a8f0fca8897b048e046f
Frank Nusko <nusko@secianus.de>
2024-01-17 13:49:00 +0100

    Update search_in_files.ps1

--- search_in_files.ps1
+++ search_in_files.ps1
@@ -1,11 +1,11 @@
-$Path = “enter the path you want to search”
-$Text = “enter the text you want to search for in those files”
+$Path = "enter the path you want to search"
+$Text = "enter the text you want to search for in those files"
 $PathArray = @()
-$Results = “C:\temp\filenames.txt”
+$Results = "C:\temp\filenames.txt"
 
-# This code snippet gets all the files in $Path that end in “.dat”.
-Get-ChildItem $Path -Filter “*.dat” -Recurse |
-Where-Object { $_.Attributes -ne “Directory”} |
+# This code snippet gets all the files in $Path that end in ".dat".
+Get-ChildItem $Path -Filter "*.dat" -Recurse |
+Where-Object { $_.Attributes -ne "Directory"} |
 ForEach-Object {
 If (Get-Content $_.FullName | Select-String -Pattern $Text) {
 $PathArray += $_.FullName
